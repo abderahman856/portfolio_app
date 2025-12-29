78e0dd70455951bb24524e46e7e88152bf0e2794
Baashe <baashe@Baashes-MacBook-Air.local>
2025-12-29 20:35:19 +0300

    Back to stable version

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,4 +1,12 @@
+output "alb_security_group_id" {
+  value = aws_security_group.alb_sg.id
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.tg.arn
+}
+
 output "alb_dns_name" {
-  description = "The DNS name of the load balancer"
   value = aws_lb.alb.dns_name
 }
+
