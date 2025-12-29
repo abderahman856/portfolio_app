8808e88029707d5868da99790fc4ac5d43793b9c
Baashe <baashe@Baashes-MacBook-Air.local>
2025-12-29 20:16:46 +0300

    Final link between alb and cloudfront

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,12 +1,3 @@
-output "alb_security_group_id" {
-  value = aws_security_group.alb_sg.id
-}
-
-output "target_group_arn" {
-  value = aws_lb_target_group.tg.arn
-}
-
-# This is the important one for CloudFront!
 output "alb_dns_name" {
   description = "The DNS name of the load balancer"
   value       = aws_lb.portfolio_alb.dns_name
