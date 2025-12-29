e57b12e0c95f2e3e08b8de941a59d39fe1390dad
Baashe <baashe@Baashes-MacBook-Air.local>
2025-12-29 20:26:09 +0300

    Removing CloudFront and resetting to working version

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,4 +1,11 @@
+output "alb_security_group_id" {
+  value = aws_security_group.alb_sg.id
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.tg.arn
+}
+
 output "alb_dns_name" {
-  description = "The DNS name of the load balancer"
-  value       = aws_lb.portfolio_alb.dns_name
+  value = aws_lb.alb.dns_name
 }
